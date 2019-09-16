5e39f3cde529bb0fd9529e095e5797aaabb18c17
bernabe <bernnabe@gmail.com>
2019-09-16 11:05:06 -0300

    Change alphabet table from 0..1 to morse symbols

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace vscode_lab
             var telegrapher = new Telegrapher();
 
             Console.WriteLine(telegrapher.SetMode(Telegrapher.Mode.AlphabeticalToMorse).Execute("HELLO WORLD"));
-            Console.WriteLine(telegrapher.SetMode(Telegrapher.Mode.MorseToAlphabetical).Execute("1111 1 1011 1011 000 / 100 000 101 1011 011"));
+            Console.WriteLine(telegrapher.SetMode(Telegrapher.Mode.MorseToAlphabetical).Execute(".... . .-.. .-.. --- / .-- --- .-. .-.. -.."));
         }
     }
 }
